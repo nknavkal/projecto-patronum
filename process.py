cf4cec68cd54256bb8c32a6b5fb8e9277a6afc11
Nishaad Navkal <nknavkal@gmail.com>
2017-11-18 14:24:42 -0800

    works so far, processed input and made outerSet

--- process.py
+++ process.py
@@ -2,20 +2,27 @@
 
 def readFile(inputFile):
     fileobj = open(inputFile, "r")
-    print(fileobj.readlines())
+    a = list(fileobj)
+    numNames = int(a[0].split()[0])
+    numConstraints = int(a[1].split()[0])
+    constraints = []
+    for i in a[2:]:
+        constraints.append(i.split()[:3])
+    #print(constraints)
+    return(makeOutsidePairs(numNames, constraints))
 
-def alphaTup(string1 string2):
-    return tuple(min(string1, string2), max(string1, string2))
+def alphaTup(string1, string2):
+    return tuple([min(string1, string2), max(string1, string2)])
 
-def makeOutsidePairs(fileArray):
+def makeOutsidePairs(numNames, constraints):
     nameSet = set()
     outerSet = set()
-    numWizards = int(fileArray[0])
-    for i in range(2, fileArray.len):
-        stringArr = i.split()
-        for j in stringArr:
-            outerSet.add(j)
-        if len(nameSet) == numWizards:
+    for constraint in constraints:
+        for element in constraint:
+            nameSet.add(element)
+        if len(nameSet) == numNames:
             break
-    for name1 in nameSet():
-        for name2 in nameSet():
+    for name1 in nameSet:
+        for name2 in nameSet:
+            outerSet.add(alphaTup(name1, name2))
+    print(outerSet)
