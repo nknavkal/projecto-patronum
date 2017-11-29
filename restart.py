dfd33ae6e485bd3b8ffa8d9cd89360a56df01ca5
jrharika <jrharika@gmail.com>
2017-11-29 02:34:33 -0800

    done?

--- restart.py
+++ restart.py
@@ -1,3 +1,5 @@
+from random import *
+from math import *
 def readFile(inputFile):
     fileobj = open(inputFile, "r")
     a = list(fileobj)
@@ -21,6 +23,55 @@ def getThirds(constraints, wizzNames):
         appearsInThird[constraint[1]] += 1
         #appearsInThird[constraint[2]] += 1
     return appearsInThird
+
+def loopOnce(guess, constraints, wizzNames):
+    bestCorrect = tester(guess, constraints)
+    for i in wizzNames:
+        iLocation = guess.index(i)
+        bestIndex = iLocation
+        for j in range(len(guess)):
+            guess[j], guess[iLocation] = i, guess[j]
+            k = tester(guess, constraints)
+            if k > bestCorrect:
+                bestCorrect = k
+                bestIndex = j
+		if k == len(constraints):
+		     break
+            guess[j], guess[iLocation] = guess[iLocation], guess[j]
+	guess[bestIndex], guess[iLocation] = i, guess[bestIndex]
+    return guess
+
+def swapTest(guess, constraints, i, j):
+    guess[j], guess[i] = guess[i], guess[j]
+    k = tester(guess, constraints)
+    guess[j], guess[i] = guess[i], guess[j]
+    return k
+
+def loopWithProb(guess, constraints):
+    origSat = tester(guess, constraints)
+    numTrys = len(constraints) / 100
+    repeats = []
+    changeInSat = {}
+    for _ in range(numTrys):
+	a = randint(0,len(guess)-1)
+        b = randint(0,len(guess)-1)
+        satisfiedAfterSwapping = swapTest(guess, constraints, a, b)
+        changeInSat[(a,b)] = satisfiedAfterSwapping - origSat
+    makeProbs(changeInSat)
+    for swap, sat in changeInSat.items():
+	for i in range(sat):
+	    repeats.append(swap)
+    selected = choice(repeats)
+    guess[selected[0]], guess[selected[1]] = guess[selected[1]], guess[selected[0]]
+    return tester(guess, constraints)
+
+def makeProbs(changeInSat):
+    for a,b in changeInSat.items():
+	changeInSat[a] = abs(b)
+    return changeInSat
+
+	
+
 
 def getWizzNames(constraints, numNames):
     nameSet = set()
@@ -53,6 +104,8 @@ def main(fileName):
     inThird = getThirds(constraints, wizzNames)
     
     ret = startingOrder(inThird)
+    while loopWithProb(ret, constraints) < len(constraints):
+	print(loopWithProb(ret, constraints))
     
 
     print(ret)
@@ -61,7 +114,20 @@ def main(fileName):
 
 
 
-
+def findFailed(guess, constraints):
+    #guess is an array
+    wrong = []
+    for constraint in constraints:
+        first = constraint[0]
+        second = constraint[1]
+        third = constraint[2]
+        lower = min(guess.index(first), guess.index(second))
+        upper = max(guess.index(first), guess.index(second))
+        if guess.index(third) < lower or guess.index(third) > upper:
+            pass
+	else:
+	    wrong.append(constraint)
+    return wrong
 
 
 
