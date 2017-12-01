4385857a47ef169d4328b2f8094b2476ac776e21
Nishaad Navkal <nknavkal@gmail.com>
2017-12-01 15:31:38 -0800

    alg works, adding outputs

--- solver.py
+++ solver.py
@@ -1,3 +1,10 @@
+import argparse
+import numpy as np
+"""
+======================================================================
+  Complete the following function.
+======================================================================
+"""
 import random
 from math import *
 from datetime import datetime
@@ -174,8 +181,8 @@ def megaMain(path):
 def main(fileName):
     start_time = time.time()
     constraints, numNames = readFile(fileName)
-    #safety = constraints[:]
-    #ret = 0
+    safety = constraints[:]
+    ret = 0
     #ordering = []
     wizzNames = getWizzNames(constraints, numNames)
     inThird = getThirds(constraints, wizzNames)
@@ -191,7 +198,7 @@ def main(fileName):
 
     print(ret)
     print("--- %s seconds ---" % (time.time() - start_time))
-    print((tester(ret, constraints)))
+    print((tester(ret, safety)))
 
 
 
@@ -223,3 +230,60 @@ def testUnsat(guess, constraints):
         else:
             failed.append(constraint)
     return failed
+
+def solve(num_wizards, num_constraints, wizards, constraints):
+    """
+    Write your algorithm here.
+    Input:
+        num_wizards: Number of wizards
+        num_constraints: Number of constraints
+        wizards: An array of wizard names, in no particular order
+        constraints: A 2D-array of constraints,
+                     where constraints[0] may take the form ['A', 'B', 'C']i
+
+    Output:
+        An array of wizard names in the ordering your algorithm returns
+    """
+    ret = np.random.choice(wizards, num_wizards, False).tolist()
+    temp = []
+    temp.append(.5)
+    while loopWithProb(ret, constraints, temp) < len(constraints):
+        pass
+    print(tester(ret, constraints))
+    return ret
+
+"""
+======================================================================
+   No need to change any code below this line
+======================================================================
+"""
+
+def read_input(filename):
+    with open(filename) as f:
+        num_wizards = int(f.readline())
+        num_constraints = int(f.readline())
+        constraints = []
+        wizards = set()
+        for _ in range(num_constraints):
+            c = f.readline().split()
+            constraints.append(c)
+            for w in c:
+                wizards.add(w)
+
+    wizards = list(wizards)
+    return num_wizards, num_constraints, wizards, constraints
+
+def write_output(filename, solution):
+    with open(filename, "w") as f:
+        for wizard in solution:
+            f.write("{0} ".format(wizard))
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description = "Constraint Solver.")
+    parser.add_argument("input_file", type=str, help = "___.in")
+    parser.add_argument("output_file", type=str, help = "___.out")
+    args = parser.parse_args()
+
+    num_wizards, num_constraints, wizards, constraints = read_input(args.input_file)
+    solution = solve(num_wizards, num_constraints, wizards, constraints)
+    write_output(args.output_file, solution)
